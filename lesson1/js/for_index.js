3707fe17967e2b89eede3e2c757fd18eea63e012
H-hidetaka <sansanhidetaka@gmail.com>
2021-06-21 21:06:57 +0900

    [Add]3-57for_num

---
+++ lesson1/js/for_index.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        isbn: '978-4-7981-5757-3',
+        title: 'JavaScript逆引きレシピ',
+        price: 2800
+      },
+      {
+        isbn: '978-4-7321-5757-3',
+        title: 'JavaScript逆ピ',
+        price: 2800
+      },
+      {
+        isbn: '978-4-7981-5757-3',
+        title: 'JavaScript逆引きレシピ',
+        price: 2800
+      },
+    ]
+  }
+});
---
+++ lesson1/js/for_num.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  // data: {
+    // book: {
+    //     isbn: '978-4-7981-5757-3',
+    //     title: 'JavaScript逆引きレシピ',
+    //     price: 2800
+    //   }
+      // {
+      //   isbn: '978-4-7321-5757-3',
+      //   title: 'JavaScript逆ピ',
+      //   price: 2800
+      // },
+      // {
+      //   isbn: '978-4-7981-5757-3',
+      //   title: 'JavaScript逆引きレシピ',
+      //   price: 2800
+      // },
+  // }
+});
